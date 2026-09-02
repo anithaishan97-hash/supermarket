490242948754cd01117376f95bd663f15122cf6d
anithaishan97-hash <anithaishan97@gmail.com>
2026-09-02 15:32:56 +0530

    fix errors

--- src/Home/Home9.jsx
+++ src/Home/Home9.jsx
@@ -3,7 +3,7 @@ import Button from '../Components/Button'
 import h9css from '../Home/Home9.module.css'
 const Home9 = () => {
   return (
-     <div className='pas' style={{display:'flex',justifyContent:'center',marginTop:'40px',marginTop:'90px'}}>
+     <div className='pas' style={{display:'flex',justifyContent:'center'}}>
      <div className='wra' style={{display:'flex',justifyContent:'center',alignItems:'center',gap:'40px',width:'90%'}}>
         <div className={h9css.pastas} style={{width:'50%',padding:'10px',lineHeight:1}}>
           <p style={{fontSize:'15px',fontWeight:'bold'}}>Taste of Italy</p>
